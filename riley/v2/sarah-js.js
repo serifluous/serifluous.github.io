43d28ed8bc1f1b9a010adecac23d97e89f699e02
Feeny <li.sarah.y@gmail.com>
2016-06-07 02:14:23 -0700

    Responsiveness

--- riley/v2/sarah-js.js
+++ riley/v2/sarah-js.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 	$('.box-work').hover(function() {
-		$('h2' , this).css("color","#E39B17");
+		$('h2' , this).css("color","#EFAB76");
 		$('.box-work__image' , this).css("background-color","#F2F2F2")
 	},function() {
-		$('h2' , this).css("color","#515151");
+		$('h2' , this).css("color","#F19650");
 		$('.box-work__image' , this).css("background-color","#EEE")
 	});
 
@@ -12,4 +12,15 @@ $(document).ready(function() {
 	},function() {
 		$(this).switchClass("em-exclamation","em-grey_question");
 	});
+
+	var identities = [
+		"can&rsquo;t stop running",
+		"codes like a girl",
+		"refuses to wear pants",
+		"made you look",
+		"can be found at Boba Guys",
+		"provides free succulent-naming services"
+		];
+	var randIdentity = identities[Math.floor( Math.random() * identities.length )];
+	$('#random-header').html( randIdentity ).text();
 });
