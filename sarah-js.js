ab818dbc6bdcdf99954d03ae91c360ed87708d8e
Feeny <li.sarah.y@gmail.com>
2016-12-27 15:20:17 -0800

    Updated JS

--- sarah-js.js
+++ sarah-js.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
 	$('.box-work').hover(
-		if ($('h2', this).hasClass('coming-soon') == false) {
 		function() {
-		$('h2' , this).css("color","#EFAB76");
-		$('.box-work__image' , this).css("background-color","#F2F2F2");
-	},function() {
-		$('h2' , this).css("color","#F19650");
-		$('.box-work__image' , this).css("background-color","#EEE");
-	}});
+			$('h2' , this).css("color","#EFAB76");
+			$('.box-work__image' , this).css("background-color","#F2F2F2");
+		},function() {
+			$('h2' , this).css("color","#F19650");
+			$('.box-work__image' , this).css("background-color","#EEE");
+	});
 
 	var identities = [
 		"can&rsquo;t stop running",
